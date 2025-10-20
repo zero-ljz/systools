e16321aae0011ae9b2ac1c467856d78e3b367039
zero-ljz <2267719005@qq.com>
2025-10-21 00:01:07 +0800

    opt

--- file_explorer/app.py
+++ file_explorer/app.py
@@ -338,4 +338,4 @@ if __name__ == "__main__":
     parser.add_argument('--port', '-p', type=int, default=8000, help='Port to listen on (default: 8000)')
     args = parser.parse_args()
 
-    app.run(host=args.host, port=args.port, debug=True, reloader=True, server='cheroot')
+    app.run(host=args.host, port=args.port, debug=True, reloader=False, server='cheroot')
